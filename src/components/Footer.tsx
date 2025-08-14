3cad037aa0683f8732146c72cbdec14dd727deed
Omar ElMoataz <komar56734@gmail.com>
2025-08-14 05:28:24 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="border-t border-gray-400">
-      <div className="container py-10 text-center text-sm text-gray-600">
+      <div className="py-10 text-center text-sm text-gray-600">
         © {new Date().getFullYear()} CineScope. All rights reserved.
       </div>
     </footer>
